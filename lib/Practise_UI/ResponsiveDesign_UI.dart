15ff9236132f7d83cd90006d13aa960aa8dd1eec
Imran Hossain <imran125651@gmail.com>
2024-08-18 21:19:46 +0600

    imran

---
+++ lib/Practise_UI/ResponsiveDesign_UI.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveDesignUI extends StatefulWidget {
+  const ResponsiveDesignUI({super.key});
+
+  @override
+  State<ResponsiveDesignUI> createState() => _ResponsiveDesignUIState();
+}
+
+class _ResponsiveDesignUIState extends State<ResponsiveDesignUI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
