982f8d0b69265efb37a00ad0fe7cd986cd671415
Imran Hossain <imran125651@gmail.com>
2024-08-20 22:04:08 +0600

    imran

--- lib/Practise_UI/ResponsiveDesign_UI.dart
+++ lib/Practise_UI/ResponsiveDesign_UI.dart
@@ -10,6 +10,49 @@ class ResponsiveDesignUI extends StatefulWidget {
 class _ResponsiveDesignUIState extends State<ResponsiveDesignUI> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print(MediaQuery.of(context).size.aspectRatio);
+    print(MediaQuery.of(context).size.flipped);
+    print(MediaQuery.of(context).devicePixelRatio);
+
+    Size size = MediaQuery.of(context).size;
+    if(size.width < 640){
+      print(" This is a Phone");
+    }
+    else if(size.width > 640 && size.width < 1008){
+      print("This is tablet");
+    }
+    else{
+      print("LAPTOP/DESKTOP");
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Responsive Design"),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Column(
+            children: [
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                alignment: WrapAlignment.center,
+                spacing: 11
+                4,
+                runSpacing: 5,
+                children: [
+                  Text("Lorem Ipsum "),
+                  Text("text of thindustry. "),
+                  Text("Lorem Ipsum has been the industry's "),
+                  Text("standard dummy text ever since the 1500s, when "),
+                  Text("an unknown printer took a galley of ty type specimen book"),
+
+                  Text(orientation.name)
+                ],
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
