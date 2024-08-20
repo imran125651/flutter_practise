48dfea78b0d2f2e37a3d511333ae66ee0cd07370
Imran Hossain <imran125651@gmail.com>
2024-08-20 20:04:32 +0600

    imran

--- lib/Practise_UI/ResponsiveDesign_UI.dart
+++ lib/Practise_UI/ResponsiveDesign_UI.dart
@@ -10,6 +10,23 @@ class ResponsiveDesignUI extends StatefulWidget {
 class _ResponsiveDesignUIState extends State<ResponsiveDesignUI> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Responsive Design"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text("Lorem Ipsum is simply dummy"),
+              Text("text of the printing and typesetting industry. "),
+              Text("Lorem Ipsum has been the industry's "),
+              Text("standard dummy text ever since the 1500s, when"),
+              Text("an unknown printer took a galley of type and scrambled it to make a type specimen book"),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
